e3bc100441877a506051845cab5d70731c945925
owlbeck <owlbeck@yahoo.com>
2019-01-02 14:53:55 +0900

    :sparkles: 個人用と公開用ノートをRead/Writeできるようにした close #7

--- components/Mypage.vue
+++ components/Mypage.vue
@@ -5,11 +5,18 @@
     <div id="notesIndex" v-for="note in notes" :key="note.content">
       <p class="noteContent">{{ note.content }}</p>
     </div>
+    <h2>公開ノート</h2>
+    <div id="notesIndex" v-for="public_note in public_notes" :key="public_note.content">
+      <p class="noteContent">{{ public_note.content }}</p>
+    </div>
     <p>
       <textarea v-model="note_content"></textarea>
     </p>
     <p>
       <button @click="saveContent(note_content)">ノートを保存する</button>
+    </p>
+    <p>
+      <button @click="savePublicContent(note_content)">公開ノートに保存する</button>
     </p>
     <button @click="logout">ログアウト</button>
   </div>
@@ -23,18 +30,20 @@ export default {
   data(context) {
     return {
       note_content: "hello",
-      notes: []
+      notes: [],
+      public_notes: []
     };
   },
   created: function() {
     this.getContent();
+    this.getPublicContent();
   },
   methods: {
     // ログアウト
     logout: function() {
       firebase.auth().signOut();
     },
-    // Firebaseからデータを取得する
+    // 自分のノートを取得する
     getContent() {
       firebase
         .database()
@@ -46,7 +55,19 @@ export default {
           }
         });
     },
-    // Firebaseにコンテンツを保存する
+    // 公開ノートを取得する
+    getPublicContent() {
+      firebase
+        .database()
+        .ref("notes/public")
+        .once("value")
+        .then(result => {
+          if (result.val()) {
+            this.public_notes = result.val();
+          }
+        });
+    },
+    // 自分のノートにコンテンツを保存する
     saveContent: function(value) {
       var newNoteKey = firebase
         .database()
@@ -59,6 +80,23 @@ export default {
         .set({ content: value });
 
       this.getContent();
+    },
+    // 公開ノートにコンテンツを保存する
+    savePublicContent: function(value) {
+      var newNoteKey = firebase
+        .database()
+        .ref()
+        .child("notes")
+        .push().key;
+      firebase
+        .database()
+        .ref("notes/public/" + newNoteKey)
+        .set({
+          content: value,
+          uid: this.user.uid
+        });
+
+      this.getPublicContent();
     }
   }
 };
